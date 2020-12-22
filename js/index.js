4decfed5c75cd77a0fa10111e5d7dff100709de2
JetStormRage <75043067+JetStormRage@users.noreply.github.com>
2020-12-22 22:29:05 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function destroy() {
+    document.querySelector('html').innerHTML = '';
+    document.querySelector('html').style.backgroundImage = "url(../img/shrek.jpg)";
+    document.querySelector('html').style.backgroundSize = '10%';
+    
+       
+}
+
+const buttonDestroy = document.querySelector('.dont-click')
+buttonDestroy.addEventListener('click', destroy);
